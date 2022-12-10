3b033c9a23fd83f7ecca97933da71870bd464386
Amir.sh <mobilefactory012@gmail.com>
2022-12-10 04:04:00 +0330

    Add : Base url react app ENV

--- src/index.js
+++ src/index.js
@@ -1,28 +1,17 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
-require("dotenv").config();
-
-console.log(process.env.BASE_URL);
-
-console.log(`connecting to port 5000`);
-
-// axios.get("https://api.amir_shirani.ir");
-
-// 1.Clean code
-// 2.Security
+reportWebVitals();
